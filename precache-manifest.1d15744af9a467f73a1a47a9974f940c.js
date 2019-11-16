229dc3802d7bc637b309948eb64954904e2f611c
alexscub <alexscub1@gmail.com>
2019-11-16 13:07:28 +0200

    Updates

---
+++ precache-manifest.1d15744af9a467f73a1a47a9974f940c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823b47563774326061e4005e71b5e048",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "731a1fb75483053121f0",
+    "url": "/goit-react-hw-04-reader/static/css/main.d0bc6bda.chunk.css"
+  },
+  {
+    "revision": "bd4f751b2b486943cb9d",
+    "url": "/goit-react-hw-04-reader/static/js/2.fdb5e8da.chunk.js"
+  },
+  {
+    "revision": "731a1fb75483053121f0",
+    "url": "/goit-react-hw-04-reader/static/js/main.0fc2b714.chunk.js"
+  },
+  {
+    "revision": "1828d4e570bc41837a2e",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.a9df6b2e.js"
+  }
+]);
